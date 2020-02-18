5e2d492e61c7f94188349f5117ae371c61eb911a
CristoferPortela <cristoferportela@gmail.com>
2020-02-18 14:17:07 -0300

    generic initial content added

---
+++ src/components/Body/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+    return(
+        <article>
+            <h2>
+                Meus trabalhos
+            </h2>
+            <section>    
+                <p>
+                    Alguns trablhos aqui
+                </p>
+            </section>
+        </article>
+    );
+}
---
+++ src/components/Nav/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './style.sass';
+
+export default () => {
+    return(
+        <nav>
+            <section>
+                <img/>
+                <h1>
+                    Cristofer Portela
+                </h1>
+            </section>
+            <section>
+                <ul>
+                    <li>
+                        Projetos
+                    </li>
+                    <li>
+                        Trabalhos
+                    </li>
+                    <li>
+                        Soluções
+                    </li>
+                    <li>
+                        Preços
+                    </li>
+                </ul>
+            </section>
+        </nav>
+    );
+}
