3f2439a7663e9c6997b90272380f929778e1fd58
CristoferPortela <cristoferportela@gmail.com>
2020-02-28 11:12:09 -0300

    Routes added

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Main from './components/Body/Main';
+import Main from './components/Body/Main/index';
+import Contact from './components/Body/Contact/index';
+import Posts from './components/posts';
 
 export default () => {
     return(
         <Switch>
             <Route path="/" exact component={Main} />
+            <Route path="/contato" component={Contact} />
+            <Route path="/pricing" />
+
+            <Route path="/plus/projects" component={Posts} />
+            <Route path="/plus/works" component={Posts} />
+            <Route path="/plus/soluctions" component={Posts} />
+
+            <Route path="/plus/:path" component={Posts} />
         </Switch>
     );
 }
