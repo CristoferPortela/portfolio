35d748ba97eb9ef9d683dc6b0754e6db7e74246f
CristoferPortela <cristoferportela@gmail.com>
2020-02-28 11:16:11 -0300

    blog content file

---
+++ src/blog/index.js
@@ -0,0 +1,116 @@
+import React from 'react';
+
+export default {
+    'intitucionais-blogs': {
+        title: "Sites intitucionais e blogs",
+        content: <div className="container">
+            <p>
+                Os blogs são conhecidos no mundo todo, tanto pela facilidade de usar 
+                e manter um, quanto pelas forma de mostrar o conteúdo, para fazer blogs,
+                não podemos esquecer do nosso grande amigo o WordPress, a maior ferramenta
+                de criação de blogs que existe! Estima-se que cerca de 30% de todos os sites
+                foram feitos usando o WordPress! Um blog é uma ótima opção para qualquer tipo
+                de necessidade!
+            </p>
+            <p>
+                Quer um site para sua empresa? Isso é uma ótima ideia! A internet nos trás
+                centenas de beneficios, desde ser um meio de comunicação ativo com os clientes
+                até fazer uma primeira etapa de seleção de candidatos para trabalhar, algo que
+                muitas empresas já fazem!
+            </p>
+            <p>
+                Há uma imensidão de benefícios em se ter um site hoje, assim como antigamente
+                usava-se muito o jornal empresso, hoje em dia usamos a internet numa escala
+                muito maior, veja na lsita abaixo algumas vantagens:
+            </p>
+            <ul>
+                <li>
+                    Comunicação com clientes ativos; 
+                </li>
+                <li>
+                    Mostrar o que a empresa está fazendo;
+                </li>
+                <li>
+                    Maior contato com potênciais futuros clientes;
+                </li>
+                <li>
+                    Um sistema de gerenciamento da empresa acessível de qualquer lugar (mesmo que não se encaixe tanto na ideia de blog, é um otimo motivo para pensar em ir para a internet);
+                </li>
+                <li>
+                    Receber feedback sobre produtos e serviços;
+                </li>
+            </ul>
+            <div>
+                <p>
+                    Viu só? Com uma pesquisa rápida na internet podemos ver ainda mais motivos,
+                    e não só isso, nessa pesquisa, podemos ter certeza de duas coisas:
+                </p>
+                <ol> 
+                    <li> 
+                        Os sites que pesquisamos são de alguma empresa que provavelmente não
+                        conhecemos e estamos tendo o primeiro contato, e; 
+                    </li>
+                    <li>
+                        Essas empresas não resolveram colocar um site ali sem motivo
+                        nenhum, eles viram as vantagens de ter um site, nada existe 
+                        sem motivo algum, mesmo que não entendamos eles de primeira.
+                    </li>
+                </ol>
+            </div>
+            <h5>
+                Se interessou?
+            </h5>
+            <p>
+                Se interessou em ter um site? Seja ele blog, seja ele institucional, ou
+                qualquer sistema web? Por que não conversamos mais sobre isso pelo 
+                email <a href="mailto:cristoferportela@gmail.com">
+                    cristoferportela@gmail.com
+                </a> ou 
+                pelo número 42 984179233.
+            </p>
+        </div>
+    },
+    ecommerce: {
+        title: "Loja virtual",
+        content: <div className="container">
+            <p>
+                O comercio surgiu cedo na história da humanidade, uma pessoa 
+                que tinha determinado produto trocava por outro que ela precisava,
+                com o tempo teve a evolução dele até chegarmos ao dia de hoje.
+            </p>
+            <p>
+                Tem um produto? Está fazendo sucesso na sua região vendendo ele? Ou
+                talvez pretende iniciar com vendas online sem ter um ponto fixo de vendas,
+                (algo bem em alto hoje em dia), se a respsota é sim, por quê não trabalhamos
+                juntos nessa?
+            </p>
+            <h4>Qual a proposta?</h4>
+            <p>
+                Antes de tudo o que está incluso nessa minha proposta? Primeiro,
+                a criação de um site de vendas online, feito com a tecnologia de
+                sua escolha e se não tiver nenhuma preferencia, então vamos 
+                conversar sobre e decidir juntos a melhor tecnologia para seus 
+                objetivos e vamos usar ela. 
+            </p>
+            <p>
+                Do que adianta ter o site mais lindo do mundo, com todas as funcionalidades
+                com uma proposta inovadora, se ninguém conhece ele? Pense na Amazon,
+                Mercado Livre e etc., será que eles só criaram o site e sumiram? Não,
+                eles investiram em marketing, a minha proposta para qualquer trabalho de 
+                ecommerce é lançar o primeiro impulsionamento dele sem nenhum custo 
+                adicional, o que acha?
+            </p>
+            <p>
+                Mais alguma vantagem em trabalharmos juntos para fazer uma loja virtual?
+                Com certeza! O que acha de um desconto para criarmos um App da loja?
+                Os aplicativos trazem o beneficio de ser acessivel com um clique
+                pela tela inicial do celular. Algo muito vantajoso para muitas lojas,
+                quer um exemplo simples? Pense numa pizzaria, você pode colocar ela
+                em apps de lanchonetes, mas isso nem sempre é um boa ideia, seu cliente 
+                pode querer ir comprar de outra pizzaria com preço mais baixo ou 
+                um sabor diferente, tendo seu próprio app, ele pode apenas clicar aqui e
+                ali e pedir de sua pizzaria.  
+            </p>
+        </div>
+    }
+}
