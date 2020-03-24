8dfc5153e0635a5614ffb1a86330510ac6f61941
CristoferPortela <cristoferportela@gmail.com>
2020-03-24 11:50:51 -0300

    showContent useState added, float-content tag added

--- src/components/Body/FloatButton.js
+++ src/components/Body/FloatButton.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default ({ bg, icon }) => {
+
+    const [showContent, setShowContent] = useState(false);
+
     return(
-        <button className={`float-button ${bg}`} type="button">
+        <>
+        <button className={`float-button ${bg}`} type="button" onClick={() => setShowContent(!showContent)}>
             <i className="material-icons white">
                 { icon }
             </i>
         </button>
+        <div className={`float-content ${showContent ? 'show' : 'hide'}`}>
+            
+        </div>
+        </>
     );
 }
